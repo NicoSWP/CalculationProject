3ac3744445444ae296a031a807f4fae5b177847f
Nico <NicoLang@hak-reutte.ac.at>
2024-02-05 13:05:15 +0100

    first commit

--- src/Calculation.java
+++ src/Calculation.java
@@ -9,10 +9,11 @@ public class Calculation {
      * @return maximum number of array
      */
     public static int findMax(int arr[]){
-        int max=0;
-        for(int i=1;i<arr.length;i++){
-            if(max<arr[i])
-                max=arr[i];
+        int max = 0;
+        for(int i=1;i<=arr.length; i++){
+            if(max<arr[i]){
+                max = arr[i];
+            }
         }
         return max;
     }
--- src/CalculationTest.java
+++ src/CalculationTest.java
@@ -1,12 +1,19 @@
-
+import org.testng.annotations.Test;
+import org.testng.Assert;
 public class CalculationTest {
+    @Test
     public void testFindMax(){
-        Calculation c = new Calculation();
-        int[] arr = new int[]{1,2,3,4,5};
-
+        int[] arr = new int[]{1,2,3,4,5}; //Normaler Testfall
         int expected = 5;
-        int result = c.findMax(arr);
 
-        //Assert.assertEquals(expected, result);
+        int[] arr2 = new int[]{5,2,3,4,1}; //Position
+        int expected2 = 5;
+
+        int[] arr3 = new int[]{5,2,3,4,5}; //Duplikation
+        int expected4 = 5;
+
+        int result = Calculation.findMax(arr);
+
+        Assert.assertEquals(expected, result);
     }
 }
